ae1190951b4cbcc1cb7f4ab80d898a5321f15a68
chebupelka <119750562+chebupelka8@users.noreply.github.com>
2023-12-25 18:57:09 +0500

    Add files via upload

--- scripts/image.py
+++ scripts/image.py
@@ -140,6 +140,48 @@ class ImageEditor:
 
         return Image(__image.image.subsurface(x, y, width, height))
 
+class AnimationEditor:
+
+    @staticmethod
+    def __verify_animation(__animation) -> None:
+        if not isinstance(__animation, Animation): raise TypeError("Argument should be an instance of the 'Animation' class")
+
+    @classmethod
+    def rotate(cls, __animation: Animation, degrees: int) -> Animation:
+        cls.__verify_animation(__animation)
+
+        return Animation(list(map(lambda i: ImageEditor.rotate(i, degrees), __animation.directory)))
+    
+    @classmethod
+    def resize(cls, __animation: Animation, width: int, height: int) -> Animation:
+        cls.__verify_animation(__animation)
+
+        return Animation(list(map(lambda i: ImageEditor.resize(i, width, height), __animation.directory)))
+    
+    @classmethod
+    def mult_size(cls, __animation: Animation, mult_width: int, mult_height: int) -> Animation:
+        cls.__verify_animation(__animation)
+
+        return Animation(list(map(lambda i: ImageEditor.mult_size(i, mult_width, mult_height), __animation.directory)))
+    
+    @classmethod
+    def set_alpha(cls, __animation: Animation, alpha: int) -> Animation:
+        cls.__verify_animation(__animation)
+
+        return Animation(list(map(lambda i: ImageEditor.set_alpha(i, alpha), __animation.directory)))
+
+    @classmethod
+    def flip(cls, __animation: Animation, side: str) -> Animation:
+        cls.__verify_animation(__animation)
+
+        return Animation(list(map(lambda i: ImageEditor.flip(i, side), __animation.directory)))
+    
+    @classmethod
+    def cut(cls, __animation: Animation, x: int | float, y: int | float, width: int, height: int) -> Animation:
+        cls.__verify_animation(__animation)
+
+        return Animation(list(map(lambda i: ImageEditor.cut(i, x, y, width, height), __animation.directory)))
+
 class Tileset:
 
     @staticmethod
@@ -167,27 +209,4 @@ class Tileset:
         
         return Animation(res)
 
-
-# def split_tilesize(self, tile_size: tuple[int, int]) -> list[pygame.Surface]:
-#         width, height = self.size[0], self.size[1]
-#         count_x, count_y = (width // tile_size[0], height // tile_size[1])
-
-#         images = []
-
-#         for y in range(count_y):
-#             for x in range(count_x):
-#                 images.append(self.image.subsurface(pygame.Rect(x * tile_size[0], y * tile_size[1], tile_size[0], tile_size[1])))
-        
-#         return images
-
-#     def split_count(self, count: tuple[int, int]) -> list[pygame.Surface]:
-#         count_x, count_y = (count[0], count[1])
-#         tile_size = (self.size[0] // count_x, self.size[1] // count_y)
-
-#         images = []
-
-#         for y in range(count_y):
-#             for x in range(count_x):
-#                 images.append(self.image.subsurface(pygame.Rect(x * tile_size[0], y * tile_size[1], tile_size[0], tile_size[1])))
-        
-#         return images
+    
