a328b5995b4a09530f9346fabe8959f554600bb1
chebupelka <119750562+chebupelka8@users.noreply.github.com>
2023-12-27 18:58:05 +0500

    Add files via upload

--- scripts/shape.py
+++ scripts/shape.py
@@ -1,5 +1,7 @@
 import pygame
 from Engine.scripts.math import Vec2, Size
+from Engine.scripts.collision import Collider
+from Engine.scripts.group import SpriteGroup
 
 
 class RectangleShape:
@@ -22,3 +24,56 @@ class RectangleShape:
     @property
     def position(self) -> Vec2:
         return Vec2(self.__rectangle.x, self.__rectangle.y)
+
+class CollisionRectangle(RectangleShape):
+    def __init__(self, position: Vec2, width: int, height: int) -> None:
+        super().__init__(position, width, height)
+
+        self.__movement = Vec2(0, 0)
+        self.__collide_groups = None
+        self.__collide_side = {
+            "top": False,
+            "bottom": False,
+            "left": False,
+            "right": False
+        }
+    
+    @property
+    def movement(self) -> Vec2:
+        return self.__movement
+
+    @movement.setter
+    def movement(self, __movement: Vec2) -> None:
+        self.__movement = __movement
+    
+    def set_collision_groups(self, *__groups: SpriteGroup) -> None:
+        self.__collide_groups = [*__groups]
+    
+    def get_collision_side(self, __side: str) -> bool:
+        return self.__collide_side.get(__side)
+    
+    def update(self) -> None:
+        # horizontal
+        self.rectangle.x += self.movement.x
+        
+        if self.__collide_groups != None:
+            for sprite in Collider.group_collider(self, *self.__collide_groups):
+                if self.movement.x > 0:
+                    self.rectangle.right = sprite.rectangle.left
+                    # self.__collide_side["right"] = True
+                if self.movement.x < 0:
+                    self.rectangle.left = sprite.rectangle.right
+                    # self.__collide_side["left"] = True
+        
+        # vertical
+        self.rectangle.y += self.movement.y
+
+        if self.__collide_groups != None:
+            for sprite in Collider.group_collider(self, *self.__collide_groups):
+                if self.movement.y > 0:
+                    self.rectangle.bottom = sprite.rectangle.top
+                    # self.__collide_side["bottom"] = True
+                if self.movement.y < 0:
+                    self.rectangle.top = sprite.rectangle.bottom
+                    # self.__collide_side["top"] = True
+        
--- scripts/sprites.py
+++ scripts/sprites.py
@@ -1,60 +1,26 @@
 import pygame
 from Engine.scripts.image import Image, Animation
 from Engine.scripts.math import Vec2
-from Engine.scripts.shape import RectangleShape
+from Engine.scripts.shape import RectangleShape, CollisionRectangle
 from Engine.scripts.collision import Collider
 from Engine.scripts.group import SpriteGroup
 
 
-class StaticSprite(RectangleShape):
+class StaticSprite(CollisionRectangle):
     def __init__(self, position: Vec2, image: Image) -> None:
         super().__init__(position, image.size.x, image.size.y)
 
         self.__image = image
-        self.__movement = Vec2(0, 0)
-        self.__collide_groups = None
-    
-    @property
-    def movement(self) -> Vec2:
-        return self.__movement
-
-    @movement.setter
-    def movement(self, __movement: Vec2) -> None:
-        self.__movement = __movement
-    
-    def collide(self, *__groups: SpriteGroup) -> None:
-        self.__collide_groups = [*__groups]
     
     def draw(self, __display: pygame.Surface) -> None:
         
-        self.__update()
+        self.update()
         __display.blit(self.__image.image, self.rectangle)
-    
-    def __update(self) -> None:
-        # horizontal
-        self.rectangle.x += self.movement.x
-        
-        if self.__collide_groups != None:
-            for sprite in Collider.group_collider(self, *self.__collide_groups):
-                if self.movement.x > 0:
-                    self.rectangle.right = sprite.rectangle.left
-                if self.movement.x < 0:
-                    self.rectangle.left = sprite.rectangle.right
-        
-        # vertical
-        self.rectangle.y += self.movement.y
-
-        if self.__collide_groups != None:
-            for sprite in Collider.group_collider(self, *self.__collide_groups):
-                if self.movement.y > 0:
-                    self.rectangle.bottom = sprite.rectangle.top
-                if self.movement.y < 0:
-                    self.rectangle.top = sprite.rectangle.bottom
         
     def __repr__(self) -> str:
         return f"StaticSprite(position={self.position}, image={self.__image})"
 
-class AnimatedSprite(RectangleShape):
+class AnimatedSprite(CollisionRectangle):
     def __init__(self, position: Vec2, animation: Animation) -> None:
         super().__init__(position, animation[0].size.x, animation[0].size.y)
 
@@ -63,28 +29,16 @@ class AnimatedSprite(RectangleShape):
         self.__animation: Animation = animation
         self.__frame = 0
         self.__is_freezed: bool = False
-        self.__movement = Vec2(0, 0)
-        self.__collide_groups = None
     
     @staticmethod
     def __verify(__animation) -> None:
         if not isinstance(__animation, Animation): raise TypeError("Argument should be an instanse of the 'Animtion' class")
     
     def draw(self, __display: pygame.Surface) -> None:
-        self.__update()
+        self.size = self.__animation[int(self.__frame)].size
+        self.update()
 
         __display.blit(self.__animation[int(self.__frame)].image, self.position.xy)
-    
-    @property
-    def movement(self) -> Vec2:
-        return self.__movement
-
-    @movement.setter
-    def movement(self, __movement: Vec2) -> None:
-        self.__movement = __movement
-    
-    def collide(self, *__groups: SpriteGroup) -> None:
-        self.__collide_groups = [*__groups]
     
     def animating(self, __speed: int | float) -> None:
         if not self.__is_freezed:
@@ -96,29 +50,6 @@ class AnimatedSprite(RectangleShape):
     
     def unfreeze(self) -> None:
         self.__is_freezed = False
-    
-    def __update(self) -> None:
-        self.size = self.__animation[int(self.__frame)].size
-
-        # horizontal
-        self.rectangle.x += self.movement.x
-        
-        if self.__collide_groups != None:
-            for sprite in Collider.group_collider(self, *self.__collide_groups):
-                if self.movement.x > 0:
-                    self.rectangle.right = sprite.rectangle.left
-                if self.movement.x < 0:
-                    self.rectangle.left = sprite.rectangle.right
-        
-        # vertical
-        self.rectangle.y += self.movement.y
-
-        if self.__collide_groups != None:
-            for sprite in Collider.group_collider(self, *self.__collide_groups):
-                if self.movement.y > 0:
-                    self.rectangle.bottom = sprite.rectangle.top
-                if self.movement.y < 0:
-                    self.rectangle.top = sprite.rectangle.bottom
     
     def __repr__(self) -> str:
         return f"AnimatedSprite(position={self.position}, animation={self.__animation}, frame={int(self.__frame)})"
